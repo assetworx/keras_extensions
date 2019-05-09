18b832b6d3395cab8f29a93b27339de7a9f48bcf
Christian Versloot <mail@christianversloot.nl>
2019-05-09 16:45:00 +0200

    Add E-Swish activation function

--- activations.py
+++ activations.py
@@ -6,3 +6,10 @@ from keras import backend as K
 #
 def swish(x):
   return x * K.sigmoid(x)
+
+#
+# E-SWISH ACTIVATION FUNCTION
+# @rationale
+# @url https://arxiv.org/pdf/1801.07145.pdf
+def eswish(x, B = 1):
+  return B * x * K.sigmoid(x)
