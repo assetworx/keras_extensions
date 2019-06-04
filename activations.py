99bb52ca9b3c2c7f05f2138efbce3d5ba9d9df92
Christian Versloot <mail@christianversloot.nl>
2019-06-04 12:06:12 +0200

    Add sigmoid util

--- activations.py
+++ activations.py
@@ -1,4 +1,5 @@
-from keras import backend as K
+import numpy as np
+from .utils import sigmoid
 
 #
 # SWISH ACTIVATION FUNCTION
@@ -6,11 +7,18 @@ from keras import backend as K
 # @url https://arxiv.org/pdf/1710.05941.pdf
 #
 def swish(x):
-  return x * K.sigmoid(x)
+  return x * sigmoid(x)
 
 #
 # E-SWISH ACTIVATION FUNCTION
 # @rationale
 # @url https://arxiv.org/pdf/1801.07145.pdf
 def eswish(x, B = 1):
-  return B * x * K.sigmoid(x)
+  return B * x * sigmoid(x)
+
+#
+# SINERELU ACTIVATION FUNCTION
+# @rationale
+# @url https://medium.com/@wilder.rodrigues/sinerelu-an-alternative-to-the-relu-activation-function-e46a6199997d
+def sinerelu(x, epsilon = 0.025):
+  return x if x > 0 else epsilon*(np.sin(x) - np.cos(x))
